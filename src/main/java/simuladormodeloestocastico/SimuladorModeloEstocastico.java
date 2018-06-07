b9f04697f3d155398a60ccfdf8ddd05468c2f320
marinalaux <laux.marina@gmail.com>
2018-06-06 21:48:14 -0300

    Gerado número aleatório conforme probabilidades.

--- src/main/java/simuladormodeloestocastico/SimuladorModeloEstocastico.java
+++ src/main/java/simuladormodeloestocastico/SimuladorModeloEstocastico.java
@@ -1,6 +1,7 @@
 package simuladormodeloestocastico;
 
 import org.apache.commons.math3.distribution.AbstractRealDistribution;
+import org.apache.commons.math3.distribution.EnumeratedIntegerDistribution;
 import org.apache.commons.math3.distribution.ExponentialDistribution;
 import org.apache.commons.math3.distribution.NormalDistribution;
 import org.apache.commons.math3.random.JDKRandomGenerator;
@@ -15,6 +16,26 @@ public class SimuladorModeloEstocastico {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        // O tempo de produção será gerado com base na distribuição de frequências (comportamento) identificado
+        
+        // A pontuação da tarefa será gerada de forma aleatória, conforme uma probabilidade
+        
+        // 
+        int[] numsToGenerate = new int[]{1, 2, 3, 5, 8};
+        double[] discreteProbabilities = new double[]{0.35, 0.35, 0.25, 0.01, 0.04};
+
+        EnumeratedIntegerDistribution distribution = new EnumeratedIntegerDistribution(numsToGenerate, discreteProbabilities);
+        
+        for (int i = 0; i < 50; i++) {
+
+            int sample = distribution.sample();
+
+            System.out.println(sample);            
+        }
+        
+
+        
         /*
             Dev 177:
                 Pontos: Média(2.25)
@@ -26,13 +47,13 @@ public class SimuladorModeloEstocastico {
                 Pontos: Média(2.60)
                 Tempos: Média(202.28) */     
         
-        RandomGenerator rand = new JDKRandomGenerator(10);//rand será utilizado para mexer com o seed
-        //Distribuições para tempos
-        ExponentialDistribution exp = new ExponentialDistribution(/*rand, */295.57); 
-        //Tentativa de distribuição para pontos do dev 187
-        NormalDistribution normal   = new NormalDistribution(/*rand */ 2.90, 1.40);
-        
-        printSamples(normal);
+//        RandomGenerator rand = new JDKRandomGenerator(10);//rand será utilizado para mexer com o seed
+//        //Distribuições para tempos
+//        ExponentialDistribution exp = new ExponentialDistribution(/*rand, */295.57); 
+//        //Tentativa de distribuição para pontos do dev 187
+//        NormalDistribution normal   = new NormalDistribution(/*rand */ 2.90, 1.40);
+//        
+//        printSamples(normal);
     }
     
     public static void printSamples(AbstractRealDistribution dist){
